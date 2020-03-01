4fb5d389ae0fcb8591e10f9270e0677c17642ca3
Thomas Albrighton <thomasalbrighton@gmail.com>
2020-03-02 09:16:07 +1030

    fixed deployment when in prod

--- routes/console.php
+++ routes/console.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Foundation\Inspiring;
+use Illuminate\Support\Facades\Artisan;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,15 @@ use Illuminate\Foundation\Inspiring;
 |
 */
 
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->describe('Display an inspiring quote');
+Artisan::command('refresh', function () {
+    try {
+        $this->info('Deleting database');
+        unlink(database_path('database.sqlite'));
+    } finally {
+        $this->info('Creating new database');
+        touch(database_path('database.sqlite'));
+
+        Artisan::call('migrate', ['--force' => true], $this->getOutput());
+        Artisan::call('db:seed', ['--force' => true], $this->getOutput());
+    }
+})->describe('Refresh the whole database');
